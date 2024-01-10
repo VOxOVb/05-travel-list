02d5ce98b511be6ad2a0d428386de2fffab40769
oxo <oxo4work@gmail.com>
2024-01-10 16:35:09 +0800

    finish

---
+++ src/component/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form"
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItems(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItems(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirm = window.confirm("Are you sure you want to delete all items?");
+    if (confirm) setItems([]);
+  }
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItems={handleDeleteItems}
+        onToggleItems={handleToggleItems}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} onClearList={handleClearList} />
+    </div>
+  );
+}
+
+
+
+
